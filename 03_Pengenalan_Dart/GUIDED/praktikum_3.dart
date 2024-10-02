a75f2b89a6c81dc4cb92f69716401e4b912998c6
Kholil Abdi Prasetiyo <136038498+kholilapras@users.noreply.github.com>
2024-10-02 21:20:52 +0700

    Laporan Praktikum 3

---
+++ 03_Pengenalan_Dart/GUIDED/praktikum_3.dart
@@ -0,0 +1,45 @@
+void main() {
+// Variable
+  // String firstName = 'Praktikum';
+  // var lastName = 'PPB';
+  // final cuaca = 'Cerah';
+
+  // var open = 8;
+  // var close = 17;
+  // var now = 13;
+
+  // // If
+  // if (now >= open && now < close) {
+  //   print("Toko Buka");
+  // } else if (now == 13) {
+  //   print("Toko Sedang Istirahat");
+  // } else {
+  //   print("Toko Tutup");
+  // }
+
+  // print('Hari ini $firstName $lastName');
+
+
+//Switch Case
+  // var nilai = 'b';
+  // switch (nilai) {
+  //   case 'a':
+  //     print('Nilai Sangat Bagus');
+  //     break;
+  //   case 'b':
+  //     print('Nilai Bagus');
+  //     break;
+  //   case 'c':
+  //     print('Nilai Cukup');
+  //     break;
+  //   default:
+  //     print("Nilai tidak tersedia");
+  // }
+
+
+//Looping
+for (int i = 1; i <= 5; i++) {
+  print(i);
+  }
+ 
+}
